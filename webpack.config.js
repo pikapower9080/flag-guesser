736925528935f1e97552954119abe596f63b4fa5
pikapower9080 <pikapower9080@gmail.com>
2022-11-29 11:50:22 -0500

    🩹 Change webpack build mode to production (oops!)

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const config = {
     entry: './src/index.js',
     watch: true,
-    mode: 'development',
+    mode: 'production',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'main.js',
