e8e139160e9ed232b3bbd57d075430a129115d57
pikapower9080 <pikapower9080@gmail.com>
2022-12-06 15:51:31 -0500

    🔨 Use a public folder instead of a json file What was I thinking?

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,24 @@
 const fs = require('fs')
 const path = require('path')
-const copyFiles = JSON.parse(fs.readFileSync('copy.json').toString())
+const copyFiles = fs.readdirSync("public")
 
 if (!fs.existsSync('build')) {
+    console.log("Creating build folder...")
     fs.mkdirSync("build")
 } else {
+    console.log("Clearing build folder...")
     fs.rmSync("build", {recursive: true})
     fs.mkdirSync("build")
 }
+
+console.log("Copying public files...")
 copyFiles.forEach((file) => {
-    if (fs.existsSync(file)) {
-        fs.cpSync(file, path.resolve('build', file), {recursive: true})
+    if (fs.existsSync(path.resolve('public', file))) {
+        fs.cpSync(path.resolve('public', file), path.resolve('build', file), {recursive: true})
     } else {
         console.warn("File or directory does not exist: " + file)
     }
 })
+console.log("Copying dist...")
+fs.cpSync('dist', path.resolve('build', 'dist'), {recursive: true})
+console.log("Done")
