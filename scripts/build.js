457f0c8f359b5f20cd448add91a03384d0a350ca
pikapower9080 <pikapower9080@gmail.com>
2022-12-06 16:06:27 -0500

    Revert "🔨 Use a public folder instead of a json file"
    This reverts commit e8e139160e9ed232b3bbd57d075430a129115d57.

--- scripts/build.js
+++ scripts/build.js
@@ -1,24 +1,17 @@
 const fs = require('fs')
 const path = require('path')
-const copyFiles = fs.readdirSync("public")
+const copyFiles = JSON.parse(fs.readFileSync('copy.json').toString())
 
 if (!fs.existsSync('build')) {
-    console.log("Creating build folder...")
     fs.mkdirSync("build")
 } else {
-    console.log("Clearing build folder...")
     fs.rmSync("build", {recursive: true})
     fs.mkdirSync("build")
 }
-
-console.log("Copying public files...")
 copyFiles.forEach((file) => {
-    if (fs.existsSync(path.resolve('public', file))) {
-        fs.cpSync(path.resolve('public', file), path.resolve('build', file), {recursive: true})
+    if (fs.existsSync(file)) {
+        fs.cpSync(file, path.resolve('build', file), {recursive: true})
     } else {
         console.warn("File or directory does not exist: " + file)
     }
 })
-console.log("Copying dist...")
-fs.cpSync('dist', path.resolve('build', 'dist'), {recursive: true})
-console.log("Done")
