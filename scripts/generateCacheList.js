6c8ab154cd4035bd4fca5984c92fa48a03052c3e
pikapower9080 <pikapower9080@gmail.com>
2022-12-13 14:12:39 -0500

    📦 Self host FontAwesome so icons work offline

--- scripts/generateCacheList.js
+++ scripts/generateCacheList.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-let directories = ['icons', 'data', 'css', 'dist', 'flags']
-let list = ['/', 'gallery.html', 'manifest.json', 'https://fonts.googleapis.com/css2?family=Alexandria:wght@500&family=Open+Sans&display=swap', 'https://fonts.gstatic.com/s/opensans/v34/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0B4gaVI.woff2', 'https://kit.fontawesome.com/9d23c8a337.js']
+let directories = ['icons', 'data', 'css', 'dist', 'flags', 'fontawesome/css', 'fontawesome/webfonts']
+let list = ['/', 'gallery.html', 'manifest.json', 'https://fonts.googleapis.com/css2?family=Alexandria:wght@500&family=Open+Sans&display=swap', 'https://fonts.gstatic.com/s/opensans/v34/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0B4gaVI.woff2']
 
 directories.forEach((directory) => {
     fs.readdirSync(path.resolve('.', directory)).forEach((file) => {
